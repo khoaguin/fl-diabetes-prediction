de32f891f198d3b9a0e311b654e0bc51ec2a727d
Khoa Duy Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-05-13 11:50:36 +0700

    add xpu device

--- fl-diabetes-prediction/fl_diabetes_prediction/task.py
+++ fl-diabetes-prediction/fl_diabetes_prediction/task.py
@@ -18,6 +18,8 @@ def get_device():
         return torch.device("cuda")
     elif torch.backends.mps.is_available():
         return torch.device("mps")
+    elif torch.xpu.is_available():
+        return torch.device("xpu")
     else:
         return torch.device("cpu")
 
